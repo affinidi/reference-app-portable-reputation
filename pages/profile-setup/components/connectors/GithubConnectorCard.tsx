4ca9e5aca230626bf3f5dd05882150a2c541d949
Lloyd Grogan <lloydgrogan@affinidi.com>
2023-01-23 13:36:59 +0100

    feat: MR comments

--- pages/profile-setup/components/connectors/GithubConnectorCard.tsx
+++ pages/profile-setup/components/connectors/GithubConnectorCard.tsx
@@ -12,27 +12,25 @@ interface Props {
 
 const GithubConnectorCard: FC<Props> = ({ isChecked, setIsChecked }) => {
   return (
-    <>
-      <Card className="col-12 col-sm-4">
-        <S.CardHeader direction="row" alignItems="center">
-          <S.CardIcon>
-            <GithubIcon />
-          </S.CardIcon>
-          <Typography variant="h6">Github</Typography>
-          <S.CardCheckboxContainer justifyContent="flex-end" direction="row">
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={() => setIsChecked(!isChecked)}
-            />
-          </S.CardCheckboxContainer>
-        </S.CardHeader>
-        <Typography variant="p1">
-          Harnessed for productivity. Designed for collaboration. Celebrated for
-          built-in security.
-        </Typography>
-      </Card>
-    </>
+    <Card className="col-12 col-sm-4">
+      <S.CardHeader direction="row" alignItems="center">
+        <S.CardIcon>
+          <GithubIcon />
+        </S.CardIcon>
+        <Typography variant="h6">Github</Typography>
+        <S.CardCheckboxContainer justifyContent="flex-end" direction="row">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          />
+        </S.CardCheckboxContainer>
+      </S.CardHeader>
+      <Typography variant="p1">
+        Harnessed for productivity. Designed for collaboration. Celebrated for
+        built-in security.
+      </Typography>
+    </Card>
   );
 };
 
