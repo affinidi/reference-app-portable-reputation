a8825605e88ec90a137bd8afbc50764ac15097cd
Vitaly Rudenko <vitaly.r@affinidi.com>
2023-01-27 14:29:51 +0200

    fix: added first test as a proof of concept

--- __tests__/pages/api/profiles/get-vcs.test.ts
+++ __tests__/pages/api/profiles/get-vcs.test.ts
@@ -1,17 +1,98 @@
-import { createMocks } from 'node-mocks-http';
-import { handler } from '../../../../pages/api/profiles/get-vcs';
+import { createMocks } from "node-mocks-http";
+import { cloudWalletClient } from "../../../../pages/api/clients/cloud-wallet-client";
+import { handler } from "../../../../pages/api/profiles/get-vcs";
+import { VerifiableCredential } from "../../../../types/vc";
 
-describe('/api/profiles/get-vcs', () => {
-  it('should respond with profile VCs', async () => {
-    const { req, res } = createMocks({ method: 'GET' });
+function createFakeVc(input: {
+  type: string;
+  issuanceDate: Date;
+}): VerifiableCredential {
+  return {
+    "@context": ["http://example.com/jsonld"],
+    type: ["VerifiableCredential", input.type],
+    issuanceDate: input.issuanceDate.toISOString(),
+    id: "fake-vc",
+    holder: {
+      id: "did:fake",
+    },
+    credentialSubject: { hello: "world" },
+    credentialSchema: {
+      type: "FakeType",
+      id: "http://example.com/json",
+    },
+  };
+}
+
+describe("/api/profiles/get-vcs", () => {
+  const accessToken = 'fake-access-token'
+
+  it("should respond with found profile VCs", async () => {
+    const githubVc = createFakeVc({
+      type: "GithubProfile",
+      issuanceDate: new Date("2020-01-03"),
+    })
+
+    jest
+      .spyOn(cloudWalletClient, "getCredentials")
+      .mockResolvedValue({
+        vcs: [
+          createFakeVc({
+            type: "SomeOtherType",
+            issuanceDate: new Date("2020-01-05"),
+          }),
+          githubVc,
+          createFakeVc({
+            type: "GithubProfile",
+            issuanceDate: new Date("2020-01-01"),
+          }),
+          createFakeVc({
+            type: "GithubProfile",
+            issuanceDate: new Date("2020-01-02"),
+          }),
+        ],
+      });
+
+    const { req, res } = createMocks({
+      method: "GET",
+      headers: {
+        authorization: accessToken,
+      },
+    });
 
     await handler(req, res);
 
     expect(res._getStatusCode()).toBe(200);
-    expect(JSON.parse(res._getData())).toEqual(
-      expect.objectContaining({
-        message: 'Your favorite animal is dog',
-      }),
-    );
+    expect(JSON.parse(res._getData())).toEqual({
+      vcs: {
+        github: githubVc,
+      }
+    });
+  });
+
+  it("should respond with empty object when no profile VCs found", async () => {
+    jest
+      .spyOn(cloudWalletClient, "getCredentials")
+      .mockResolvedValue({
+        vcs: [
+          createFakeVc({
+            type: "SomeOtherType",
+            issuanceDate: new Date("2020-01-05"),
+          }),
+        ],
+      });
+
+    const { req, res } = createMocks({
+      method: "GET",
+      headers: {
+        authorization: accessToken,
+      },
+    });
+
+    await handler(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData())).toEqual({
+      vcs: {}
+    });
   });
 });
