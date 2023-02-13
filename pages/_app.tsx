efc6e65b099dcb59749565e1b7c0c9fba0d15955
ezgi.a <ezgi.a@affinidi.com>
2023-02-13 17:07:38 +0100

    feat: removed extra line

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
