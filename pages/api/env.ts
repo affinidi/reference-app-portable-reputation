15e1a2a3afa0d81c9435533792b53bc5b843a537
ezgi.a <ezgi.a@affinidi.com>
2023-02-13 16:55:33 +0100

    feat: added env variables check

--- pages/api/env.ts
+++ pages/api/env.ts
@@ -1,5 +1,22 @@
 // backend-only envs
 
+(function requireEnvs() {
+  const names: string[] = [
+    'PROJECT_ID',
+    'PROJECT_DID',
+    'API_KEY_HASH',
+    'GITHUB_APP_CLIENT_ID',
+    'GITHUB_APP_CLIENT_SECRET',
+  ]
+
+  const missingEnvs: string[] = names.filter((name) => !process.env[name])
+  if (missingEnvs.length !== 0) {
+    throw new Error(
+       `Required envs are not provided: ${missingEnvs.join(', ')}. Please check README file.`
+    )
+  }
+}) ()
+
 export const cloudWalletApiUrl = process.env.CLOUD_WALLET_API_URL
 export const affinidiIamApiUrl = process.env.AFFINIDI_IAM_API_URL
 export const issuanceApiUrl = process.env.ISSUANCE_API_URL
@@ -13,5 +30,3 @@ export const githubClientId = process.env.GITHUB_APP_CLIENT_ID
 export const githubClientSecret = process.env.GITHUB_APP_CLIENT_SECRET
 
 export const logLevel = process.env.LOG_LEVEL || 'info'
-
-
