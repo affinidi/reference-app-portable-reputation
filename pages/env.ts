5cb4b1d5769ee734d9da8a42d32eca3f487dcf49
ezgi.a <ezgi.a@affinidi.com>
2023-02-14 10:06:55 +0100

    feat: added env variables control

--- pages/env.ts
+++ pages/env.ts
@@ -1,3 +1,8 @@
 // shared envs for frontend and backend
 
 export const hostUrl = process.env.NEXT_PUBLIC_HOST
+
+if (!hostUrl) {
+    throw new Error('NEXT_PUBLIC_HOST environment variable is undefined, please follow instructions in README to setup the application')
+  }
+
